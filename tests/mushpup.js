626b0391336a50bba463498605df32827dd51ee0
klenwell <klenwell@gmail.com>
2015-07-12 16:52:43 -0700

    Stubs out Mushpup unit tests.

---
+++ tests/mushpup.js
@@ -0,0 +1,12 @@
+module("Mushpup Test 1");
+
+test("Mushpup loaded", function() {
+  ok(Mushpup, "Mushpup module found");
+});
+
+
+module("Mushpup Test 2");
+
+test("Mushpup loaded", function() {
+  ok(Mushpup, "Mushpup module found");
+});
