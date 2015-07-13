b8a30ed4c5057d44f88ef52077709c82e9a102b7
klenwell <klenwell@gmail.com>
2015-07-13 07:09:50 -0700

    Adds tests for basic hash.

--- tests/mushpup_test.js
+++ tests/mushpup_test.js
@@ -1,8 +1,23 @@
+/*
+ * Mushpup Tests
+ */
 (function() {
-  module("Mushpup module");
+  module('Mushpup module');
 
-  test("Mushpup loaded", function() {
+  test('Mushpup loaded', function() {
     ok(Mushpup);
     equal(Mushpup.version(), '3.0');
+  });
+})();
+
+(function() {
+  module('Mushpup basic hash');
+
+  test('basic hash', function() {
+    equal(Mushpup.mush('test', ''), 'qUqP5cyxm6YcTAhz05Hph5gv');
+    equal(Mushpup.mush('te', 'st'), 'qUqP5cyxm6YcTAhz05Hph5gv');
+    equal(Mushpup.mush('', 'test'), 'qUqP5cyxm6YcTAhz05Hph5gv');
+    equal(Mushpup.mush('locus', 'pocus'), 'dhjcHSkePkdoNZDZwrmZO33v');
+    equal(Mushpup.mush('mushpup.org/klenwell', 'pocus'), 'dOyw5hTH46xzHJK1N8bagrAT');
   });
 })();
