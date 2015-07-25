0287514ae9ac945cfc7a1aac52b7a1de2974f911
klenwell <klenwell@gmail.com>
2015-07-25 07:53:19 -0700

    Stubs out interface building methods.

--- src/mushpup-ui.js
+++ src/mushpup-ui.js
@@ -5,4 +5,49 @@
  *
  */
 var MushpupUI = (function() {
+
+  var VERSION = '2.0';
+
+  var init = function(parentId) {
+    var $parentElement = $('#' + parentId);
+    var $mushpupInterface = buildInterface();
+    $parentElement.append(mushpupInterface);
+    enableHandlers($parentElement);
+  };
+
+  var buildInterface = function() {
+    var $mushpupInterface = $('<div class="mushpup" />');
+    var $inputPanel = buildInputPanel();
+    var $actionPanel = buildActionPanel();
+    var $outputPanel = buildOutputPanel();
+
+    $mushpupInterface
+      .append($inputPanel)
+      .append($actionPanel)
+      .append($outputPanel);
+
+    return $mushpupInterface;
+  };
+
+  var enableHandlers = function() {
+  };
+
+  // DOM Builders
+  var buildInputPanel = function() {
+  };
+
+  var buildActionPanel = function() {
+  };
+
+  var buildOutputPanel = function() {
+  };
+
+  /*
+   * Public Interface
+   */
+  var API = {
+    init: init,
+    version: function() { return VERSION; }
+  };
+  return API;
 });
